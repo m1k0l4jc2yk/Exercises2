4b8d9e7158b85599c8a4fd2e317d011765a97c24
Krzysztof <m1k0l4jc2yk@users.noreply.github.com>
2017-12-09 18:43:37 +0100

    Ex-2

---
+++ Exercises2/DateThreeTest.java
@@ -0,0 +1,16 @@
+
+public class DateThreeTest {
+  public static void main(String args[]){
+    DateThree date = new DateThree();
+
+//    date.setDay(01);
+//    date.setMonth(07);
+//    date.setYear(1991);
+
+    date.setDate(9,12,2017);
+    date.displayDate();
+
+
+//    System.out.println("The date is: "+date.getDay() + "/" + date.getMonth() + "/" + date.getYear());
+  }
+}
