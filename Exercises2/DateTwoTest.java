148138717fdb3642d128af6f25dcb3fe4d4a3b0e
Krzysztof <m1k0l4jc2yk@users.noreply.github.com>
2017-12-09 18:43:51 +0100

    Ex-2

---
+++ Exercises2/DateTwoTest.java
@@ -0,0 +1,13 @@
+
+public class DateTwoTest {
+  public static void main(String args[]) {
+
+    /*DateTwo date = new DateTwo();
+    date.day = 1;
+    date.month = 7;
+    date.year = 1991;*/
+
+  }
+    
+}
+
