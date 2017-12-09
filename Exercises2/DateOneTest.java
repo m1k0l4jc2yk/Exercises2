831822faa64e3c609495bec6d1b39f9918884702
Krzysztof <m1k0l4jc2yk@users.noreply.github.com>
2017-12-09 18:43:24 +0100

    Ex-2

---
+++ Exercises2/DateOneTest.java
@@ -0,0 +1,16 @@
+
+public class DateOneTest {
+  public static void main(String args[]) {
+
+    DateOne date = new DateOne();
+
+    date.day = 10;
+    date.month = 9;
+    date.year = 1991;
+
+    System.out.println("Date One: " +date.day +"/" + date.month +"/" + date.year);
+    
+  }
+
+}
+
