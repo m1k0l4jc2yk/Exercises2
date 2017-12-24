333b2bc3b0c7212e25b865300ff880bc780310ec
Krzysztof <m1k0l4jc2yk@users.noreply.github.com>
2017-12-24 15:00:55 +0100

    Obj-4

---
+++ JavaStartObjektowe/FirmaDwa.java
@@ -0,0 +1,23 @@
+
+public class FirmaDwa {
+    public static void main(String args[]) {
+        PracownikDwa[] pracownicy = new PracownikDwa[4];
+
+        String[] imiona = {"Jan", "Tomasz", "Marcin", "Krzysztof"};
+        String[] nazwiska = {"Bazan", "Trawka", "Kozubek", "Kowalski"};
+        int[] wiek = {30, 20, 45, 55};
+
+        for (int i = 0; i < pracownicy.length; i++) {
+            pracownicy[i] = new PracownikDwa();
+            pracownicy[i].imie = imiona[i];
+            pracownicy[i].nazwisko = nazwiska[i];
+            pracownicy[i].wiek = wiek[i];
+        }
+
+        System.out.println("Pracownicy firmy to:");
+        for (int i = 0; i < pracownicy.length; i++) {
+            System.out.println(pracownicy[1].imie + "," + pracownicy[i].nazwisko + "," + pracownicy[i].wiek + " lat");
+        }
+    }
+
+}
