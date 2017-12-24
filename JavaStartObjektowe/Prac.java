3f862f7969da2ec3da0c0b8029dfa43a3d0bbfe5
Krzysztof <m1k0l4jc2yk@users.noreply.github.com>
2017-12-24 14:59:19 +0100

    Obj-4

---
+++ JavaStartObjektowe/Prac.java
@@ -0,0 +1,18 @@
+
+public class Prac {
+    String imie;
+    String nazwisko;
+    int wyplata;
+
+    public Prac(){
+        imie = "";
+        nazwisko = "";
+        wyplata = 0;
+    }
+
+    public Prac (String i, String n, int w){
+        imie = i;
+        nazwisko = n;
+        wyplata = w;
+    }
+}
