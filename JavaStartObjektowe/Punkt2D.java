d8eaad369f1a9c269809152c615030e622f9e89e
Krzysztof <m1k0l4jc2yk@users.noreply.github.com>
2017-12-27 17:55:08 +0100

    Obj-5

---
+++ JavaStartObjektowe/Punkt2D.java
@@ -0,0 +1,17 @@
+
+public class Punkt2D {
+    int x;
+    int y;
+
+    public Punkt2D(){
+        x=0;
+        y=0;
+    }
+    public Punkt2D(int a, int b){
+        x = a;
+        y = b;
+    }
+}
+
+
+
