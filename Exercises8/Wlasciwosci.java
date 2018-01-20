b5b5595f2ce623cdf3e5470cef5184f1f2bb157e
Krzysztof <m1k0l4jc2yk@users.noreply.github.com>
2018-01-20 20:01:35 +0100

    Ex-8

---
+++ Exercises8/Wlasciwosci.java
@@ -0,0 +1,13 @@
+package Exercises8;
+
+public class Wlasciwosci {
+
+    public static void main(String[] args)
+    {
+        System.out.println("\nCurrent system environment:");
+        System.out.println(System.getenv());
+        System.out.println("\n\nCurrent system properties:");
+        System.out.println(System.getProperties());
+    }
+}
+
