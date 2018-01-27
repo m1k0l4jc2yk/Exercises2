fe4c5f55216897532fac731afbeed5766d1e7457
Krzysztof <m1k0l4jc2yk@users.noreply.github.com>
2018-01-27 19:31:59 +0100

    Ex-8

--- Exercises8/TestLicznik.java
+++ Exercises8/TestLicznik.java
@@ -16,21 +16,24 @@ public class TestLicznik {
             wynik = wynik + usa[i]/usa.length;
         }
 
-        double [] euro = new double[5];
-        usa[0] = 4.0;
-        usa[1] = 4.55;
-        usa[2] = 4.8;
-        usa[3] = 4.95;
-        usa[4] = 4.85;
+        double [] euro = {4.0,4.55, 4.8, 4.93, 4.05 };
 
         double wynik2=0;
 
         for (int i = 0; i < euro.length; i++){
-            wynik2 = wynik2 + euro[i]/usa.length;
+            wynik2 = wynik2 + euro[i]/euro.length;
         }
 
-        if (wynik >= usa[4] && wynik2 >=euro[4]){
-            System.out.println("średnia większa od ostatniej wprowadzonej wartości:" + String.format("%.3f",wynik) +"\n Warto inwestować !!!");
+        double [] funt = {4.85,4.88, 4.90, 4.92, 4.80 };
+
+        double wynik3=0;
+
+        for (int i = 0; i < funt.length; i++){
+            wynik2 = wynik2 + funt[i]/funt.length;
+        }
+
+        if (wynik >= usa[4] && wynik2 >=euro[4] || wynik3>=funt[4]){
+            System.out.println("średnia większa od ostatniej wprowadzonej wartości:" + String.format("%.3f",wynik) +"\nWarto inwestować !!!");
         } else{
             System.out.println("Nie inwestuj w walutę USA");
         }
