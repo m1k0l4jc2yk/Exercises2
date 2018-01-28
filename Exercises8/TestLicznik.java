df2bc2f935be8d53997a7363b3f949e98b7b3fd4
Krzysztof <m1k0l4jc2yk@users.noreply.github.com>
2018-01-28 21:22:42 +0100

    Ex-8

--- Exercises8/TestLicznik.java
+++ Exercises8/TestLicznik.java
@@ -3,12 +3,17 @@ package Exercises8;
 public class TestLicznik {
 
     public static void main(String[] args) {
-        double [] usa = new double[5];
-        usa[0] = 4.32;
-        usa[1] = 4.05;
-        usa[2] = 4.55;
-        usa[3] = 4.80;
-        usa[4] = 4.32;
+        double [] usa = new double[10];
+        usa[0] = 3.401;
+        usa[1] = 3.419;
+        usa[2] = 3.4109;
+        usa[3] = 3.4108;
+        usa[4] = 3.3994;
+        usa[5] = 3.4036;
+        usa[6] = 3.4084;
+        usa[7] = 3.375;
+        usa[8] = 3.3376;
+        usa[9] = 3.3299;
 
         double wynik=0;
 
@@ -16,7 +21,7 @@ public class TestLicznik {
             wynik = wynik + usa[i]/usa.length;
         }
 
-        double [] euro = {4.0,4.55, 4.8, 4.93, 4.05 };
+        double [] euro = {4.1696,4.1825, 4.1739, 4.1663, 4,175, 4.1696, 4.1712, 4.1637, 4.1493, 4,1477};
 
         double wynik2=0;
 
@@ -24,7 +29,7 @@ public class TestLicznik {
             wynik2 = wynik2 + euro[i]/euro.length;
         }
 
-        double [] funt = {4.85,4.88, 4.90, 4.92, 4.80 };
+        double [] funt = {4.6902,4.706, 4.7051, 4.7216, 4,7301, 4.7301, 4.7527, 4.7563, 4.7688, 4,7489};
 
         double wynik3=0;
 
@@ -32,12 +37,26 @@ public class TestLicznik {
             wynik2 = wynik2 + funt[i]/funt.length;
         }
 
-        if (wynik >= usa[4] && wynik2 >=euro[4] || wynik3>=funt[4]){
-            System.out.println("średnia większa od ostatniej wprowadzonej wartości:" + String.format("%.3f",wynik) +"\nWarto inwestować !!!");
+        if (wynik <= usa[9] && (wynik2 <= euro[9] || wynik3 <=funt[9])){
+            System.out.println("Srednia mniejsza od ostatniej wartości kursu, Srednia = "  + String.format("%.3f",wynik) +"\nOstatnia wartość kursu = " +euro[9]);
+            System.out.println("\nInwestuj !!!");
         } else{
             System.out.println("Nie inwestuj w walutę USA");
         }
 
+        if (wynik2 <= euro[9] && (wynik <= usa[9] || wynik3 <=funt[9])){
+            System.out.println("Srednia mniejsza od ostatniej wartości kursu, Srednia = "  + String.format("%.3f",wynik2) +"\nOstatnia wartość kursu = " +euro[9]);
+            System.out.println("\nInwestuj !!!");
+        } else{
+            System.out.println("Nie inwestuj w walutę GB");
+        }
+
+        if (wynik3 <=funt[9] && (wynik <= usa[9] || wynik2 <= euro[9])){
+            System.out.println("Srednia mniejsza od ostatniej wartości kursu, Srednia = "  + String.format("%.3f",wynik2) +"\nOstatnia wartość kursu = " +euro[9]);
+            System.out.println("\nInwestuj !!!");
+        } else{
+            System.out.println("Nie inwestuj w walutę GB");
+        }
 
     }
 
