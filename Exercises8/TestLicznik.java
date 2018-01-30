6321c85e7f6caf11ff9c922489a1ba7f53ce4943
Krzysztof <m1k0l4jc2yk@users.noreply.github.com>
2018-01-30 21:57:17 +0100

    Ex-8

--- Exercises8/TestLicznik.java
+++ Exercises8/TestLicznik.java
@@ -3,7 +3,7 @@ package Exercises8;
 public class TestLicznik {
 
     public static void main(String[] args) {
-        double [] usa = new double[11];
+        double [] usa = new double[12];
         usa[0] = 3.401;
         usa[1] = 3.419;
         usa[2] = 3.4109;
@@ -15,6 +15,7 @@ public class TestLicznik {
         usa[8] = 3.3376;
         usa[9] = 3.3299;
         usa[10] = 3.3375;
+        usa[11] = 3.3442;
 
         double wynik=0;
 
@@ -22,7 +23,7 @@ public class TestLicznik {
             wynik = wynik + usa[i]/usa.length;
         }
 
-        double [] euro = {4.1696, 4.1825, 4.1739, 4.1663, 4.175, 4.1696, 4.1712, 4.1637, 4.1493, 4.1477, 4.1423};
+        double [] euro = {4.1696, 4.1825, 4.1739, 4.1663, 4.175, 4.1696, 4.1712, 4.1637, 4.1493, 4.1477, 4.1423, 4.1461};
 
         double wynik2=0;
 
@@ -30,7 +31,7 @@ public class TestLicznik {
             wynik2 = wynik2 + euro[i]/euro.length;
         }
 
-        double [] funt = {4.6902, 4.706, 4.7051, 4.7216, 4.7301, 4.7301, 4.7527, 4.7563, 4.7688, 4.7489, 4.7082};
+        double [] funt = {4.6902, 4.706, 4.7051, 4.7216, 4.7301, 4.7301, 4.7527, 4.7563, 4.7688, 4.7489, 4.7082, 4.7061};
 
         double wynik3=0;
 
