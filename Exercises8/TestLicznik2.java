67a2b9b5bec5567cabad0ae7997f01a0dc14bc3e
Krzysztof <m1k0l4jc2yk@users.noreply.github.com>
2018-02-01 22:15:56 +0100

    Ex-8

---
+++ Exercises8/TestLicznik2.java
@@ -0,0 +1,38 @@
+package Exercises8;
+
+public class TestLicznik2 {
+
+    public static void main(String[] args) {
+        double [] usa = new double[12];
+        usa[0] = 3.401;
+        usa[1] = 3.419;
+        usa[2] = 3.4109;
+        usa[3] = 3.4108;
+        usa[4] = 3.3994;
+        usa[5] = 3.4036;
+        usa[6] = 3.4084;
+        usa[7] = 3.375;
+        usa[8] = 3.3376;
+        usa[9] = 3.3299;
+        usa[10] = 3.3375;
+        usa[11] = 3.3442;
+
+        double srednia=0;
+
+        for (int i = 0; i < usa.length; i++){
+            srednia = srednia + usa[i]/usa.length;
+        }
+
+        double srednia2 =0;
+
+        for (int i = 0; i < funt.length; i++){
+            srednia2  = srednia2  + funt[i]/funt.length;
+        }
+        double srednia3 =0;
+
+        for (int i = 0; i < funt.length; i++){
+            srednia3  = srednia3  + funt[i]/funt.length;
+        }
+
+
+
